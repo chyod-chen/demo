8d992d3571da677e6b52cc313d71280af20f8280
chyod-chen <862309564@qq.com>
2020-03-28 02:49:10 +0800

    springboot learning day 01 学习第一天:   1.demo for hello springboot(get quick start) 做了一个快速开始的hello demo ;   2.yml format 了解了yml的语法格式,分行表示,同行表示,对象,数组,map的表示,基本上是基于键值对的形式加上空格的控制;   3.properties for bean 配置文件值注入到bean中(暂时不知道有什么实际应用);   4.尚硅谷视频第10集结束;   5.2018版IDEA创建的快速启动的springboot项目中,测试类BUG: 测试类必须是要声明为public,否则无法进行junit测试;   6.学习了github的创建项目,IDEA提交修改/创建项目

---
+++ src/main/java/com/example/demo/controller/MyRestController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <ul>
+ * <li>文件名称：MyRestController</li>
+ * <li>文件描述：暂无描述</li>
+ * <li>版权所有：版权所有(C) 2019</li>
+ * <li>公 司：厦门云顶伟业信息技术有限公司</li>
+ * <li>内容摘要：</li>
+ * <li>其他说明：</li>
+ * <li>创建日期：2020/3/28</li>
+ * </ul>
+ *
+ * <ul>
+ * <li>修改记录：</li>
+ * <li>版 本 号：</li>
+ * <li>修改日期：</li>
+ * <li>修 改 人：</li>
+ * <li>修改内容：</li>
+ * </ul>
+ *
+ * @author chenyc
+ * @version 1.0.0
+ */
+
+/*
+* 这相当于 @controller+@responsbody
+*/
+@RestController
+public class MyRestController {
+
+    //只能get方式访问
+    @GetMapping("/hello/rest")
+    public String hell(){
+         return "hello rest";
+    }
+}
