c181ba20cebd43d5434030baea3c64c10072bf86
chyod-chen <862309564@qq.com>
2020-03-28 10:25:49 +0800

    配置properties配置文件编写及自动注入到实体

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 
 import com.example.demo.bean.Person;
-import javafx.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
